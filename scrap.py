b31300ccf29a2897b9e6af18fc9a3a018f6b5c26
Yuriko Takahashi <yuricocoa3618@gmail.com>
2021-03-02 01:39:06 +0900

    second commit

--- scrap.py
+++ scrap.py
@@ -1,57 +1,47 @@
-# 事前準備
-# https://kenkoooo.com/atcoder/#/user/yurimo36?userPageTab=Submissions で
-# All表示 & Status選択 をして、開発者ツールを使って tbody のHTMLをコピーして、
-# それを scrap.html にペーストする
-
 import os
 import time
 import requests
 from bs4 import BeautifulSoup
 
-with open ('scrap.html', 'r') as f:
-  html = f.read()
-soup = BeautifulSoup(html, "html.parser")
-links = soup.select("a")
-
-with open('last_url.txt', 'r') as f:
-  last_url = f.read()
+results = requests.get('https://kenkoooo.com/atcoder/atcoder-api/results?user=yurimo36').json()
+results_sorted = sorted(results, key=lambda x:x['id'], reverse=True)
 i = 0
 
-for link in links:
-  if "Detail" in link.text.splitlines()[0]:
-    url = link["href"]
+with open('last_id.txt', 'r') as f:
+  last_id = f.read()
+
+for result in results_sorted:
+  if result["result"] == "AC":
+    id_ = str(result["id"])
+    problem_id = result["problem_id"]
+    contest_id = result["contest_id"]
+    language = result["language"]
+
     if i == 0:
-      with open('last_url.txt', mode='w') as f:
-        f.write(url)
-    if url == last_url:
+      with open('last_id.txt', mode='w') as f:
+        f.write(id_)
+    if id_ == last_id:
       break
 
+    url = "https://atcoder.jp/contests/" + contest_id + "/submissions/" + id_
     print(url)
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
 
     code = soup.select_one("pre").text#.rstrip("\n")
-    contest = url.split("/")[4]
+    url = "https://atcoder.jp/contests/" + contest_id + "/tasks/" + problem_id
 
-    for tr in soup.select("tr")[:5]:
-      if tr.select_one("th").text == "Task":
-        difficulty = tr.select_one("td").text[0].lower()
-        url = "https://atcoder.jp" + tr.select_one("a")["href"]
-      if tr.select_one("th").text == "Language":
-        language  = tr.select_one("td").text
+    if not os.path.exists(contest_id):
+      os.mkdir(contest_id)
 
-    path = contest + "/" + difficulty
-
+    path = contest_id + "/" + problem_id
     if language[0:2] == "Py":
       path += ".py"
       code = "# "+ url + "\n\n" + code #+ "\n"
     else:
       path += ".cpp"
       code = "// "+ url + "\n\n" + code #+ "\n"
-
-    if not os.path.exists(contest):
-      os.mkdir(contest)
 
     with open(path, mode='w') as f:
       f.write(code)
