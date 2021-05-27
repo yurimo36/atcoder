665470ca46be5d4d102ce89c973ef0454c9d00ab
Yuriko Takahashi <yuricocoa3618@gmail.com>
2021-05-27 01:24:40 +0000

    add new file

---
+++ abc202/abc202_c.py
@@ -0,0 +1,18 @@
+# https://atcoder.jp/contests/abc202/tasks/abc202_c
+
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+c = list(map(int,input().split()))
+d = [0 for i in range(n)]
+
+ans = 0
+
+for i in range(n):
+  x = b[c[i]-1]-1
+  d[x] += 1
+
+for i in range(n):
+  ans += d[a[i]-1]
+
+print(ans)
