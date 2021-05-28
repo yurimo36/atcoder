206d3afeaa2441b43821cd8d19b5bc1b86ef71bf
Yuriko Takahashi <yuricocoa3618@gmail.com>
2021-05-28 01:36:11 +0000

    add new file

---
+++ abc195/abc195_c.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/abc195/tasks/abc195_c
+
+n = int(input())
+x = len(str(n))
+y = 1
+ans = 0
+
+for i in range(4, 16):
+  if x > i:
+    ans += 9 * y * 10**(i-1)
+    if i%3 == 0:
+      y += 1
+  else:
+    break
+
+if x > 3:
+  ans += (n - 10**(x-1) + 1) * y
+
+print(ans)
---
+++ zone2021/zone2021_a.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/zone2021/tasks/zone2021_a
+
+s = list(input())
+ans = 0
+
+for i in range(9):
+  x = s[i:i+4]
+  if ''.join(x) == "ZONe":
+    ans += 1
+
+print(ans)
