2ec821528ef381fb86e59cf9ef6980c33ee18439
Yuriko Takahashi <yuricocoa3618@gmail.com>
2021-05-29 01:50:54 +0000

    add new file

---
+++ jsc2021/jsc2021_a.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/jsc2021/tasks/jsc2021_a
+
+x, y, z = map(int,input().split())
+
+a = y / x
+b = z * a
+
+if b.is_integer():
+  print(int(b-1))
+else:
+  print(int(b))
---
+++ jsc2021/jsc2021_b.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/jsc2021/tasks/jsc2021_b
+
+n, m = map(int,input().split())
+a = list(set(list(map(int,input().split()))))
+b = list(set(list(map(int,input().split()))))
+
+c = a + b
+ans = []
+
+for i in c:
+  if c.count(i) == 1:
+    ans.append(i)
+
+ans.sort()
+print(*ans)
