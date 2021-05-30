8971af45c8f4f8a73c74ae8124edcf02fca234c8
Yuriko Takahashi <yuricocoa3618@gmail.com>
2021-05-30 02:01:58 +0000

    add new file

---
+++ zone2021/zone2021_b.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/zone2021/tasks/zone2021_b
+
+def calc_slope_intersept(x1, y1, x2, y2):
+  a = (y1 - y2) / (x1 - x2)
+  b = y1 - a * x1
+  return b
+
+n, d, h = map(int,input().split())
+li = [list(map(int,input().split())) for i in range(n)]
+
+ans = 0
+for l in li:
+  ans = max(ans, calc_slope_intersept(l[0], l[1], d, h))
+
+print(ans)
